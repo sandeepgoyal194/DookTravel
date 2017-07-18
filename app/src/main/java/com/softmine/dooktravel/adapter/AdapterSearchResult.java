db8c3dd5d71cc921a054fb68c55305f6ef4749db
sandeepgoyal194 <sandeepgoyal194@gmail.com>
2017-07-18 23:14:33 +0530

    DookTravel App Setup

---
+++ app/src/main/java/com/softmine/dooktravel/adapter/AdapterSearchResult.java
@@ -0,0 +1,97 @@
+package com.softmine.dooktravel.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.softmine.dooktravel.R;
+import com.softmine.dooktravel.pojos.Model;
+import com.softmine.dooktravel.util.Utils;
+
+import java.util.ArrayList;
+
+/**
+ * Created by GAURAV on 7/1/2017.
+ */
+
+public class AdapterSearchResult extends RecyclerView
+        .Adapter<AdapterSearchResult
+        .DataObjectHolder> {
+    private static String LOG_TAG = "MyRecyclerViewAdapter";
+    private ArrayList<Model> mDataset;
+    private static ClickListener mClickListener;
+    static Context context;
+    public static class DataObjectHolder extends RecyclerView.ViewHolder
+            implements View
+            .OnClickListener {
+        TextView tvName;
+        TextView tvCompany;
+        TextView tvAddress;
+
+
+        public DataObjectHolder(View itemView) {
+            super(itemView);
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            tvCompany = (TextView) itemView.findViewById(R.id.tvCompany);
+            tvAddress = (TextView) itemView.findViewById(R.id.tvAddress);
+            tvName.setTypeface(Utils.getSemiBoldTypeFace(context));
+            tvCompany.setTypeface(Utils.getRegularTypeFace(context));
+            tvAddress.setTypeface(Utils.getRegularTypeFace(context));
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            mClickListener.onItemClick(getAdapterPosition(), v);
+        }
+    }
+
+    public void setOnItemClickListener(ClickListener mClickListener) {
+        this.mClickListener = mClickListener;
+    }
+
+    public AdapterSearchResult(ArrayList<Model> myDataset, Context context) {
+        mDataset = myDataset;
+        this.context=context;
+    }
+
+    @Override
+    public DataObjectHolder onCreateViewHolder(ViewGroup parent,
+                                               int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.serach_result_item, parent, false);
+
+        DataObjectHolder dataObjectHolder = new DataObjectHolder(view);
+        return dataObjectHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(DataObjectHolder holder, int position) {
+        holder.tvName.setText(mDataset.get(position).getName());
+        holder.tvCompany.setText(mDataset.get(position).getCompany());
+        holder.tvAddress.setText(mDataset.get(position).getAddress());
+
+    }
+
+    public void addItem(Model dataObj, int index) {
+        mDataset.add(index, dataObj);
+        notifyItemInserted(index);
+    }
+
+    public void deleteItem(int index) {
+        mDataset.remove(index);
+        notifyItemRemoved(index);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public interface ClickListener {
+        public void onItemClick(int position, View v);
+    }
+}
